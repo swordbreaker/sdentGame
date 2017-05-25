3a97b245784fc77f3aa7ed90ea65e86961aa3ddc
Benjamin Meyer <benikm91@gmail.com>
2017-05-25 15:40:37 +0200

    Added UnityAssets + Interaction sample

---
+++ Assets/Scripts/Interaction/InteractionController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionController : MonoBehaviour {
+
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		Debug.DrawRay (transform.position, transform.forward, Color.yellow);
+		RaycastHit hitInfo;
+		if (Physics.Raycast (transform.position, transform.forward, out hitInfo, 10, LayerMask.GetMask ("PlayerInteraction")))
+		{
+			var interaction = hitInfo.transform.gameObject.GetComponent<IInteraction> ();
+			if (interaction != null) 
+			{
+				Debug.Log (interaction.Name);
+				if (Input.GetKeyDown (KeyCode.E)) {
+					interaction.Interact (this.gameObject);
+				}
+			}
+		}
+	}
+}
