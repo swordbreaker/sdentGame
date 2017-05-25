b503e61de04b160224c0419df08826810ffb9890
Tobias Bollinger <tobias.bollinger@students.fhnw.ch>
2017-05-25 17:46:41 +0200

    Added the Engine room

---
+++ Assets/Scripts/Enviroment/EngineRoomLeft.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineRoomLeft : MonoBehaviour
+{
+    [SerializeField] private Animator[] _cylinderAnimator;
+
+	private void Start ()
+    {
+        DisableAnimations();
+	}
+
+    public void RepairEngine()
+    {
+        EnableAnimations();
+    }
+
+    private void DisableAnimations()
+    {
+        foreach (var animator in _cylinderAnimator)
+        {
+            animator.enabled = false;
+        }
+    }
+
+    private void EnableAnimations()
+    {
+        foreach (var animator in _cylinderAnimator)
+        {
+            animator.enabled = true;
+        }
+    }
+}
