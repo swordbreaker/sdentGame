8e9a599a7a1d05f910080b98349d4ab9b663f8d7
Tobias Bollinger <tobias.bollinger@students.fhnw.ch>
2017-06-03 16:04:03 +0200

    SAD

---
+++ Assets/Scripts/MainMenu/MainMenuPanel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+using ItsHarshdeep.LoadingScene.Controller;
+
+public class MainMenuPanel : MonoBehaviour
+{
+
+    [SerializeField] private float _delay;
+    [SerializeField] private Ease _ease;
+    [SerializeField] private Button _playButton;
+    [SerializeField] private Button _exitButton;
+
+    private RectTransform _rectTransform;
+
+	private void Start ()
+	{
+	    _rectTransform = GetComponent<RectTransform>();
+		Invoke("Show", _delay);
+
+	    _playButton.onClick.AddListener(() => SceneController.LoadLevel("Main"));
+	    _exitButton.onClick.AddListener(Application.Quit);
+	}
+
+    private void Show()
+    {
+        _rectTransform.DOLocalMoveX(620f, 2f).SetEase(_ease);
+    }
+}
