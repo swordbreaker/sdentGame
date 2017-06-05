db368dc64eb81899f1c9de430fdd2cff054fd5e2
Christian Scheller <ch.scheller@hotmail.com>
2017-06-05 16:19:51 +0200

    Added fancy table, fixed ambient occlusion problem (unity bug)

---
+++ Assets/Scripts/Enviroment/HologramRotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HologramRotator : MonoBehaviour
+{
+
+    [SerializeField] private Vector3 angularVelocity;
+
+	public void Update ()
+	{
+        //transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + angularVelocity*Time.deltaTime);
+        transform.Rotate(angularVelocity * Time.deltaTime);
+
+    }
+}
