f51077ed98a3a0f829bd383ae9f3d002b1eb9d30
Tobias Bollinger <tobias.bollinger@students.fhnw.ch>
2017-06-10 17:22:10 +0200

    Added Credits

---
+++ Assets/Scripts/CreditsPanel.cs
@@ -0,0 +1,17 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsPanel : MonoBehaviour
+{
+    private void Start()
+    {
+        GetComponent<RectTransform>().DOLocalMoveY(1080f, 20f);
+        Invoke("BackToMainMenu", 21f);
+    }
+
+    private void BackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
