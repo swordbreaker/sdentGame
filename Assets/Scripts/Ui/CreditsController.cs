a3000d416375210dccaaf484692bddc2d8ca8cd4
Benjamin Meyer <benikm91@gmail.com>
2018-02-03 16:33:02 +0100

    Added skip credits button.

---
+++ Assets/Scripts/Ui/CreditsController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsController : MonoBehaviour 
+{
+
+    [SerializeField] private Button skipCreditsButton;
+
+	void Start() 
+    {
+        skipCreditsButton.gameObject.SetActive(false);
+	}
+	
+	void Update() 
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            skipCreditsButton.gameObject.SetActive(true);
+        }
+	}
+
+}
---
+++ Assets/Scripts/Ui/SkipCreditsButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class SkipCreditsButton : MonoBehaviour 
+{
+
+	void Start () 
+    {
+        GetComponent<Button>().onClick.AddListener(() => SceneManager.LoadScene(0));
+	}
+
+}
