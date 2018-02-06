3bb59a7cf29df828aeea5d42ba0808f10a693e44
Benjamin Meyer <benikm91@gmail.com>
2018-02-06 12:09:20 +0100

    Add sound to Room door. Changed script to Door2.cs

---
+++ Assets/Scripts/Enviroment/Door2.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource), typeof(Animator))]
+public class Door2 : MonoBehaviour {
+    
+    private AudioSource _audioSource;
+    private Animator _anim;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            print("open");
+            Open();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Close();
+        }
+    }
+
+    private void Open()
+    {
+        _audioSource.Play();
+        _anim.SetBool("character_nearby", true);
+    }
+
+    private void Close()
+    {
+        _anim.SetBool("character_nearby", false);
+    }
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _anim = GetComponent<Animator>();
+    }
+
+}
+
