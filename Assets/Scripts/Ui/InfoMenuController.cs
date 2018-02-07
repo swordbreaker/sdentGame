c138445f9193b6aba32471b67543169b096c3880
Benjamin Meyer <benikm91@gmail.com>
2018-02-07 20:12:16 +0100

    Added info scene

---
+++ Assets/Scripts/Ui/InfoMenuController.cs
@@ -0,0 +1,16 @@
+﻿using ItsHarshdeep.LoadingScene.Controller;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoMenuController : MonoBehaviour {
+
+    [SerializeField] private Button _back;
+    [SerializeField] private Button _website;
+
+	void Start () 
+    {
+        _back.onClick.AddListener(() => SceneController.LoadLevel("MainMenu"));
+        _website.onClick.AddListener(() => Application.OpenURL("https://saiwa-d891b.firebaseapp.com/"));
+	}
+
+}
