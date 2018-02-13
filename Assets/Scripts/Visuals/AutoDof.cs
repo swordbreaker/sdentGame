76d5dde31083a6571797ae7a097b9db5d38c41b8
Tobias Bollinger <tobias.bollinger@students.fhnw.ch>
2018-02-14 00:18:11 +0100

    Added alot of detailes to the Ship.

--- Assets/Scripts/Visuals/AutoDof.cs
+++ Assets/Scripts/Visuals/AutoDof.cs
@@ -12,27 +12,29 @@ namespace Assets.Scripts.Visuals
 
         private Camera _camera;
 
-        private int _transparentFxLayer;
+        private int _playerLayer;
 
         private void Start()
         {
             _camera = Camera.main;
-            _transparentFxLayer = LayerMask.NameToLayer("TransparentFX");
+            _playerLayer = LayerMask.GetMask("Player", "TransparentFX");
         }
 
         private void FixedUpdate()
         {
             var ray = _camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            ray.origin = ray.origin + ray.direction * -0.2f;
 
             Debug.DrawRay(ray.origin, ray.direction, Color.green);
 
             //Quaternion.Slerp(transform.localRotation, newRotation, 5 * Time.deltaTime);
 
             RaycastHit hit;
-            if(Physics.Raycast(ray, out hit, 200, ~0, QueryTriggerInteraction.Ignore)) //all layer expept Transparent FX layer
+            if(Physics.Raycast(ray, out hit, 200, ~_playerLayer, QueryTriggerInteraction.Ignore)) //all layer except Player layer
             {
                 DepthOfField setting;
-                if(_postProcessrofile.TryGetSettings<DepthOfField>(out setting))
+
+                if (_postProcessrofile.TryGetSettings<DepthOfField>(out setting))
                 {
                     var dist = Mathf.Lerp(setting.focusDistance.value, Vector3.Distance(transform.position, hit.point), 0.5f);
                     //setting.focusDistance.value = Vector3.Distance(transform.position, hit.point);
